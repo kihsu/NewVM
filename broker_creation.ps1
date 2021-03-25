7952b145a03fd260920f10a76aeeee72ae4885c0
kihsu <48483017+kihsu@users.noreply.github.com>
2021-03-25 16:57:02 +0100

    Update and rename broker creation.ps1 to broker_creation.ps1

--- broker_creation.ps1
+++ broker_creation.ps1
@@ -1,22 +1,15 @@
 $clusterName     = 'Cluster38' 
-$BrokerName     = 'ReplicaBroker' 
 $BrokerIPAddress   = '10.38.0.99' 
-$BrokerResourceName = "Virtual Machine Replication Broker" 
-$TempBrokerGroupName = $BrokerName + 'Group'
+$IgnoreNetwork : "10.38.1.0/24, 10.38.2.0/24" #i didn't try this
+$BrokerName     = 'ReplicaBroker' #DO NOT CHANGE
+$BrokerResourceName = "Virtual Machine Replication Broker" #DO NOT CHANGE
+$TempBrokerGroupName = $BrokerName + 'Group' #DO NOT CHANGE
 
-Add-ClusterServerRole -Name $BrokerName -StaticAddress $BrokerIPAddress -IgnoreNetwork 10.38.1.0/24, 10.38.2.0/24 -Cluster $clusterName
-
+Add-ClusterServerRole -Name $BrokerName -StaticAddress $BrokerIPAddress -IgnoreNetwork $IgnoreNetwork -Cluster $clusterName
 Add-ClusterGroup -Name $TempBrokerGroupName -GroupType VMReplicaBroker -Cluster $clusterName
-
 Add-ClusterResource -Name $BrokerResourceName -Group $TempBrokerGroupName -ResourceType 'Virtual Machine Replication Broker' -Cluster $clusterName
-
 Move-ClusterResource -name $BrokerName -Group $TempBrokerGroupName -Cluster $clusterName 
-
 Add-ClusterResourceDependency -Resource $BrokerResourceName -Provider $BrokerName -Cluster $clusterName
-
 Remove-ClusterGroup -Name $BrokerName -RemoveResources -Force -Cluster $clusterName
-
 Get-ClusterGroup -Name $TempBrokerGroupName -Cluster $clusterName | %{ $_.Name = $BrokerName }
-
 Start-ClusterGroup -Name $BrokerName -Cluster $clusterName
-Read-Host "Appuyer sur ENTER pour finir le script"
